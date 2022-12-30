cee27677074e5382250d7bab23d15f41d1ca63f3
jatin foujdar <jatin321001@gmail.com>
2022-12-30 22:48:16 +0530

    routes comp commit

--- Backend/Routes/index.js
+++ Backend/Routes/index.js
@@ -1,8 +1,12 @@
 import  express  from "express";
 import taskRouter from "./tasks.js"
+import authRouter from "./tasks.js"
+import userRouter from "./user.js"
 
 const router = express.Router();
-router.use("/tasks",taskRouter)
 
+router.use("/tasks",taskRouter)
+router.use("/auth", authRouter)
+router.use("/user", userRouter)
 
 export default router;
