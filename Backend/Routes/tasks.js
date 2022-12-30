de4902c6ff6bac9de45cde78ec1cbe7c0546431a
jatin foujdar <jatin321001@gmail.com>
2022-12-30 22:39:28 +0530

    routes commit

---
+++ Backend/Routes/tasks.js
@@ -0,0 +1,11 @@
+import  express  from "express";
+
+
+const router =  express.Router();
+
+
+router.get("/hello",(req,res)=>{
+    res.json("hello world");
+});
+
+export default router;
--- Backend/index.js
+++ Backend/index.js
@@ -1,10 +1,26 @@
 import  express  from "express";
+import cors from "cors"
 import mongoose from "mongoose";
+import morgan from "morgan";
 import "dotenv/config"
-
+import cookieParser from "cookie-parser";
+import allRoutes from "./Routes/index.js"
 
 const PORT = process.env.PORT || 4000;
 const app = express();
+
+
+//middleware
+app.use(cors());
+app.use(morgan("tiny"));
+app.use(express.json());
+app.use(cookieParser());
+
+
+
+//Routes
+app.use("/api",allRoutes)
+
 
 const connectDB = async () =>{
     try {
